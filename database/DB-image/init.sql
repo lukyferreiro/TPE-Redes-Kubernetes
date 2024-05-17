ff87489e43a1184b8a2944d98d5691dfe67c9cf0
Roman Gomez Kiss <romgomez@itba.edu.ar>
2024-05-17 16:42:10 -0300

    Created image for external postgres db

---
+++ database/DB-image/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS  alumnos  (
+     nombre  TEXT,
+     legajo  INT,
+    PRIMARY KEY ( legajo )
+);
+
+INSERT INTO  alumnos  ( nombre ,  legajo ) VALUES ('Aleca', 60395);
+INSERT INTO  alumnos  ( nombre ,  legajo ) VALUES ('Facha', 60195);
+INSERT INTO  alumnos  ( nombre ,  legajo ) VALUES ('Tom', 60396);
+INSERT INTO  alumnos  ( nombre ,  legajo ) VALUES ('Roman', 61003);
