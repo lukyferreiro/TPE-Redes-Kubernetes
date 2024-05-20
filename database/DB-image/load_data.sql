613e4efee5ac502aa31e03787662bed522f1ac33
lukyferreiro <lferreiro@itba.edu.ar>
2024-05-20 09:15:10 -0300

    Udpate load_data

--- database/DB-image/load_data.sql
+++ database/DB-image/load_data.sql
@@ -1,4 +1,4 @@
-\COPY players(name, team, age, height, weight, college, country, draft_year, draft_round)
+\COPY players(player_name, team_abbreviation, age, player_height, player_weight, college, country, draft_year, draft_round)
 FROM '/docker-entrypoint-initdb.d/players.csv'
 DELIMITER ','
 CSV HEADER;
